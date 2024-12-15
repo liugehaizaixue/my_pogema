b14a1421bc98459223d9d7c5fb2073be56f4b28b
lyl <845602274@qq.com>
2024-12-15 16:40:10 +0800

    fix animate

--- pogema/animation.py
+++ pogema/animation.py
@@ -445,14 +445,16 @@ class AnimationMonitor(Wrapper):
             x_path = []
             y_path = []
             opacity = []
-            for agent_state in gh.history[agent_idx]:
+            for t_step , agent_state in enumerate(gh.history[agent_idx]):
                 x, y = agent_state.get_xy()
 
                 x_path.append(str(cfg.draw_start + y * cfg.scale_size))
                 y_path.append(str(-cfg.draw_start + -(gh.width - x - 1) * cfg.scale_size))
 
                 if egocentric_idx is not None:
-                    ego_x, ego_y = agent_state.get_xy()
+                    ego_agent_state = gh.history[egocentric_idx][t_step]
+                    ego_x, ego_y = ego_agent_state.get_xy()
+
                     if self.check_in_radius(x, y, ego_x, ego_y, self.grid_config.obs_radius):
                         opacity.append('1.0')
                     else:
